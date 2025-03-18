5c175b9d8a1915c3453fca22519f51ea768a2576
Douglas-it <maicon.santos.eng@outlook.com>
2025-03-18 13:03:25 +0000

    This is the Begin

---
+++ app/Models/Ticket.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    //
+   protected $fillable = ['title', 'description', 'category_id', 'priority_id', 'level_id', 'user_id', 'status', 'closed_at'];
+
+}
